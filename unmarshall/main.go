913838d6b314ccba1a4b1c75406007972babe6d7
vladshchigolev <vladshch.40@gmail.com>
2021-07-15 11:58:09 +0300

    Initial commit

---
+++ unmarshall/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+var myStruct struct {
+	name string
+	age int
+}
+
+type Users struct {
+	Users []User `json:"users"` // Типом значения поля "Users" является СЕГМЕНТ(Slice) значений типа "User", описанного ниже
+}
+
+type User struct {
+	Name   string `json:"name"`
+	Type   string `json:"type"`
+	Age    int    `json:"age"`
+	Social Social `json:"social"` // Типом значения поля "Social" является тип "Social", по сути это поле - структура в структуре
+}
+
+type Social struct {
+	Vkontakte string `json:"vkontakte"`
+	Facebook  string `json:"facebook"`
+}
+
+func main() {
+	// 1. Создаём файловый дескриптор (создаётся новый поток ввода (из файла), насколько я понимаю,
+	// ядро ОС возвращает файловый дескриптор, связанный с этим потоком)
+	jsonFile, err := os.Open("C:\\Users\\vlad\\go\\src\\specialistGolangLevel2\\unmarshall\\users.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// 2. Десериализуем содержимое users.json в экземпляр Go
+	// 2.1 Инициализируем экземпляр типа "Users"
+	var users Users
+	fmt.Println(users)
+	// 2.2 Вычитываем содержимое переменной jsonFile (точнее говоря, значение переменной - указатель на то, что там нужно)
+	// в виде последовательности байт
+	byteValue, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// 2.3 Теперь задача - перенести всё из byteValue в users - это и есть десериализация
+	fmt.Println(byteValue) // Тип byteValue - [], там лежит slice, содержащий в себе набор значений типа byte(uint8)
+	json.Unmarshal(byteValue, &users)
+	fmt.Println(users)
+	//fmt.Println("In myUser:", myUser, reflect.TypeOf(myUser))
+
+}
